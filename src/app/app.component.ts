46420e1a5efd28db6226573376cfac836dfee1df
DESKTOP-BRA2J3T\Rio <jverma@homeownersfg.com>
2023-02-02 08:35:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator-app';
+  result:string = '';
+  resultName = 'Result';
+
+  add(firstNumber:string,secondNumber:string){
+    this.resultName = 'Add Result';
+    this.result = (Number(firstNumber)+Number(secondNumber)).toString();
+  }
+
+  subtract(firstNumber:string,secondNumber:string){
+    this.resultName = 'Subtract Result';
+    this.result = (Number(firstNumber)-Number(secondNumber)).toString();
+  }
+
+  multiply(firstNumber:string,secondNumber:string){
+    this.resultName = 'Multiply Result';
+    this.result = (Number(firstNumber)*Number(secondNumber)).toString();
+  }
+  divide(firstNumber:string,secondNumber:string){
+    this.resultName = 'Divide Result';
+    this.result = (Number(firstNumber)/Number(secondNumber)).toString();
+  }
 }
